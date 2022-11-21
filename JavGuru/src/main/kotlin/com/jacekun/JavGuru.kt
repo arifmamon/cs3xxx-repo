d5437f83c6b2217dce70a2eb675b502f0ea64dd7
arifmamon <arifmamon222@gmail.com>
2022-11-21 11:58:14 +0530

    Update JavGuru.kt

--- JavGuru/src/main/kotlin/com/jacekun/JavGuru.kt
+++ JavGuru/src/main/kotlin/com/jacekun/JavGuru.kt
@@ -112,6 +112,7 @@ class JavGuru : MainAPI() {
         val title = body.select("h1.titl").text()
         val descript = body.select("div.wp-content").select("p").firstOrNull()?.text()
         val streamUrl = ""
+        val tags = document.select("div.infoleft div:nth-child(4) > a").map { it.text() }
         val year = body.select("div.infometa > div.infoleft > ul > li")
             ?.get(1)?.text()?.takeLast(10)?.substring(0, 4)?.toIntOrNull()
 
@@ -122,6 +123,7 @@ class JavGuru : MainAPI() {
             type = globaltvType,
             dataUrl = streamUrl,
             posterUrl = poster,
+            tags = tags
             year = year,
             plot = descript,
             comingSoon = true
